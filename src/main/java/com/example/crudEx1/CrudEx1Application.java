8b8ba200172b20bb431561180479410d10c9c2b2
unknown <hemapriya0440@gmail.com>
2020-07-03 15:00:40 +0530

    swagger added

--- src/main/java/com/example/crudEx1/CrudEx1Application.java
+++ src/main/java/com/example/crudEx1/CrudEx1Application.java
@@ -1,5 +1,7 @@
 package com.example.crudEx1;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
@@ -8,11 +10,40 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CrudEx1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudEx1Application.class, args);
+	}
+	@Bean
+	public Docket api() {
+		HashSet<String> consumesAndProduces = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(metadata())
+				.consumes(consumesAndProduces)
+				.produces(consumesAndProduces)
+				.pathMapping("/");
+	}
+
+	private ApiInfo metadata() {
+		return new ApiInfoBuilder()
+				.title("My awesome API")
+				.description("My awesome API Description")
+				.version("1.0")
+				.contact(new Contact("Ranga", "http://www.in28minutes.com",
+						"in28minutes@gmail.com"))
+				.license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+				.build();
 	}
 @Bean
 public LocaleResolver localeResolver() {
