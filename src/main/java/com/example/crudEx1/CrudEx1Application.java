5171ecd51ffa7faacf1f6be87eeb0762624509cf
unknown <hemapriya0440@gmail.com>
2020-07-03 18:08:36 +0530

    actuator added

--- src/main/java/com/example/crudEx1/CrudEx1Application.java
+++ src/main/java/com/example/crudEx1/CrudEx1Application.java
@@ -1,7 +1,5 @@
 package com.example.crudEx1;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
@@ -10,9 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,37 +22,23 @@ public class CrudEx1Application {
 		SpringApplication.run(CrudEx1Application.class, args);
 	}
 	@Bean
-	public Docket api() {
-		HashSet<String> consumesAndProduces = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(metadata())
-				.consumes(consumesAndProduces)
-				.produces(consumesAndProduces)
-				.pathMapping("/");
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }                                
+	    
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolvr = new AcceptHeaderLocaleResolver();
+		localeResolvr.setDefaultLocale(Locale.US);
+		return localeResolvr;
 	}
-
-	private ApiInfo metadata() {
-		return new ApiInfoBuilder()
-				.title("My awesome API")
-				.description("My awesome API Description")
-				.version("1.0")
-				.contact(new Contact("Ranga", "http://www.in28minutes.com",
-						"in28minutes@gmail.com"))
-				.license("Apache 2.0")
-				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
-				.build();
-	}
-@Bean
-public LocaleResolver localeResolver() {
-	AcceptHeaderLocaleResolver localeResolvr=new AcceptHeaderLocaleResolver();
-	localeResolvr.setDefaultLocale(Locale.US);
-	return localeResolvr;
+	/*
+	 * @Bean public ResourceBundleMessageSource bundleMessageSource() {
+	 * ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+	 * messageSource.setBasename("messages"); return messageSource; }
+	 */
 }
-/*@Bean 
-public ResourceBundleMessageSource bundleMessageSource() {
-	ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
-messageSource.setBasename("messages");
-	return messageSource;
-}*/
-}
-
